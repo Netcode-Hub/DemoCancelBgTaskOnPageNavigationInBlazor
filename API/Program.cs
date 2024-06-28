37802b1dc2285254afaa47921e6f85cf813516e8
Frederick Asante <Rastinga1995@hotmail.com>
2024-06-28 05:53:20 +0000

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(builder =>
+{
+    builder.AddDefaultPolicy(p =>
+    {
+        p.WithOrigins("https://localhost:7119");
+        p.AllowAnyHeader();
+        p.AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BlazorApp/General.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using NetcodeHub.Packages.Components.BackgroundTaskStopper;
+using System.Net.Http.Json;
+
+namespace BlazorApp
+{
+    public class General : NetcodeHubTaskManager
+    {
+        public WeatherForecast[]? forecasts;
+        [Inject] public HttpClient HttpClient { get; set; } = new();
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await HttpClient.GetFromJsonAsync<WeatherForecast[]>("https://localhost:9888/weather", ComponentMonitor());
+        }
+    }
+}
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
